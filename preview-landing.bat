548139ea5c7efa72bd567790b254a3a8f591e653
Pratik <pratik.yadav2106@gmail.com>
2026-08-27 10:02:07 +0530

    chore: add 1-click preview-landing.bat launcher

---
+++ preview-landing.bat
@@ -0,0 +1,8 @@
+@echo off
+title Static Landing Page Preview
+echo ===================================================
+echo   Launching Static Video Landing Page
+echo ===================================================
+echo.
+echo Opening index.html in your default browser...
+start "" "%~dp0index.html"
